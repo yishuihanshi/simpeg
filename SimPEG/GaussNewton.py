c9a55ed83156b5d7d0b0723c6d0f465d6027e2f2
Rowan Cockett <rowan@visiblegeology.com>
2013-08-20 23:25:19 -0700

    Generalized the GaussNewton Code

--- SimPEG/GaussNewton.py
+++ SimPEG/GaussNewton.py
@@ -4,88 +4,137 @@ from utils import mkvc, sdiag
 norm = np.linalg.norm
 
 
-def GaussNewton(fctn, x0,maxIter=20, maxIterLS=10, LSreduction=1e-4, tolJ=1e-3, tolX=1e-3,
-                                                    tolG=1e-3, eps=1e-16, xStop=[]):
-    """
-        GaussNewton Optimization
+class Minimize(object):
+    """docstring for Minimize"""
 
-        Input:
-        ------
-            fctn - objective Function (lambda function)
-            x0   - starting guess
+    name = "GeneralMinimizationAlgorithm"
 
-        Output:
-        -------
-            xOpt - numerical optimizer
-    """
-    # initial output
-    print "%s GaussNewton %s" % ('='*22, '='*22)
-    print "iter\tJc\t\tnorm(dJ)\tLS"
-    print "%s" % '-'*57
+    maxIter = 20
+    maxIterLS = 10
+    LSreduction = 1e-4
+    LSshorten = 0.5
+    tolF = 1e-4
+    tolX = 1e-4
+    tolG = 1e-4
+    eps = 1e-16
 
-    # evaluate stopping criteria
-    if xStop==[]:
-        xStop=x0
-    Jstop = fctn(xStop)
-    print "%3d\t%1.2e" % (-1, Jstop[0])
+    def __init__(self, problem, **kwargs):
+        self.problem = problem
 
-    # initialize
-    xc = x0
-    STOP = np.zeros((5,1),dtype=bool)
-    iterLS=0; iter=0
+        # Set the variables, throw an error if they don't exist.
+        for attr in kwargs:
+            if hasattr(self, attr):
+                setattr(self, attr, kwargs[attr])
+            else:
+                raise Exception('%s attr is not recognized' % attr)
 
-    Jold = Jstop
-    xOld = xc
-    while 1:
-        # evaluate objective function
-        Jc,dJ,H = fctn(xc)
-        print "%3d\t%1.2e\t%1.2e\t%d" % (iter, Jc[0],norm(dJ),iterLS)
+    def minimize(self, x0):
+
+        self.startup(x0)
+        self.printInit()
+
+        while True:
+            self.f, self.g, self.H = self.evalFunction(self.xc)
+            self.printIter()
+            if self.stoppingCriteria(): break
+            p  = self.findSearchDirection()
+            xt, passLS  = self.linesearch(p)
+            if not passLS:
+                xt = self.linesearchBreak(p)
+            self.doEndIteration(xt)
+
+        self.printDone()
+
+        return self.xc
+
+    def startup(self, x0):
+        self._iter = 0
+        self._iterLS = 0
+        self._STOP = np.zeros((5,1),dtype=bool)
+
+        self.x0 = x0
+        self.xc = x0
+        self.xOld = x0
+
+    def printInit(self):
+        print "%s %s %s" % ('='*22, self.name, '='*22)
+        print "iter\tJc\t\tnorm(dJ)\tLS"
+        print "%s" % '-'*57
+
+    def printIter(self):
+        print "%3d\t%1.2e\t%1.2e\t%d" % (self._iter, self.f, norm(self.g), self._iterLS)
+
+    def printDone(self):
+        print "%s STOP! %s" % ('-'*25,'-'*25)
+        print "%d : |fc-fOld| = %1.4e <= tolF*(1+|fStop|) = %1.4e"  % (self._STOP[0], abs(self.f-self.fOld), self.tolF*(1+abs(self.fStop)))
+        print "%d : |xc-xOld| = %1.4e <= tolX*(1+|x0|)    = %1.4e"  % (self._STOP[1], norm(self.xc-self.xOld), self.tolX*(1+norm(self.x0)))
+        print "%d : |g|       = %1.4e <= tolG*(1+|fStop|) = %1.4e"  % (self._STOP[2], norm(self.g), self.tolG*(1+abs(self.fStop)))
+        print "%d : |g|       = %1.4e <= 1e3*eps          = %1.4e"  % (self._STOP[3], norm(self.g), 1e3*self.eps)
+        print "%d : iter      = %3d\t <= maxIter\t       = %3d"     % (self._STOP[4], self._iter, self.maxIter)
+        print "%s DONE! %s\n" % ('='*25,'='*25)
+
+    def evalFunction(self, x, doDerivative=True):
+        f, g, H = self.problem(x)
+        return f, g, H
+
+    def findSearchDirection(self):
+        return -self.g
+
+    def stoppingCriteria(self):
+        if self._iter == 0:
+            self.fStop = self.f  # Save this for stopping criteria
 
         # check stopping rules
-        STOP[0] = (iter>0) & (abs(Jc[0]-Jold[0]) <= tolJ*(1+abs(Jstop[0])))
-        STOP[1] = (iter>0) & (norm(xc-xOld)      <= tolX*(1+norm(x0)))
-        STOP[2] = norm(dJ)                       <= tolG*(1+abs(Jstop[0]))
-        STOP[3] = norm(dJ)                       <= 1e3*eps
-        STOP[4] = (iter >= maxIter)
-        if all(STOP[0:3]) | any(STOP[3:]):
-            break
+        self._STOP[0] = self._iter > 0 and (abs(self.f-self.fOld) <= self.tolF*(1+abs(self.fStop)))
+        self._STOP[1] = self._iter > 0 and (norm(self.xc-self.xOld) <= self.tolX*(1+norm(self.x0)))
+        self._STOP[2] = norm(self.g) <= self.tolG*(1+abs(self.fStop))
+        self._STOP[3] = norm(self.g) <= 1e3*self.eps
+        self._STOP[4] = self._iter >= self.maxIter
+        return all(self._STOP[0:3]) | any(self._STOP[3:])
 
-        # get search direction
-        dx = np.linalg.solve(H,-dJ)
-
+    def linesearch(self, p):
         # Armijo linesearch
-        descent = np.dot(dJ.T,dx)
-        LS =0; t = 1; iterLS=1
-        while  (iterLS<maxIterLS):
-            xt = xc + t*dx
-            Jt = fctn(xt)
-            LS = Jt[0]<Jc[0]+t*LSreduction*descent
-            if LS:
+        descent = np.inner(self.g, p)
+        t = 1
+        iterLS = 0
+        while iterLS < self.maxIterLS:
+            xt = self.xc + t*p
+            ft, temp, temp = self.evalFunction(xt, doDerivative=False)
+            if ft < self.f + t*self.LSreduction*descent:
                 break
-            iterLS = iterLS+1
-            t = .5*t
+            iterLS += 1
+            t = self.LSshorten*t
 
+        self._iterLS = iterLS
+        return xt, iterLS < self.maxIterLS
+
+    def linesearchBreak(self, p):
+        raise Exception('The linesearch got broken. Boo.')
+
+    def doEndIteration(self, xt):
         # store old values
-        Jold = Jc; xOld = xc
-        # update
-        xc = xt
-        iter = iter +1
+        self.fOld = self.f
+        self.xOld, self.xc = self.xc, xt
+        self._iter += 1
 
-    print "%s STOP! %s" % ('-'*25,'-'*25)
-    print "%d : |Jc-Jold| = %1.4e <= tolJ*(1+|Jstop|) = %1.4e"  % (STOP[0],abs(Jc[0]-Jold[0]),tolJ*(1+abs(Jstop[0])))
-    print "%d : |xc-xOld| = %1.4e <= tolX*(1+|x0|)    = %1.4e"  % (STOP[1],norm(xc-xOld),tolX*(1+norm(x0)))
-    print "%d : |dJ|      = %1.4e <= tolG*(1+|Jstop|) = %1.4e"  % (STOP[2],norm(dJ),tolG*(1+abs(Jstop[0])))
-    print "%d : |dJ|      = %1.4e <= 1e3*eps          = %1.4e"  % (STOP[3],norm(dJ),1e3*eps)
-    print "%d : iter      = %3d\t <= maxIter\t       = %3d"     % (STOP[4],iter,maxIter)
-    print "%s DONE! %s\n" % ('='*25,'='*25)
 
-    return xc
+class GaussNewton(Minimize):
+    name = 'GaussNewton'
+    def findSearchDirection(self):
+        return np.linalg.solve(self.H,-self.g)
+
+
+class SteepestDescent(Minimize):
+    name = 'SteepestDescent'
+    def findSearchDirection(self):
+        return -self.g
+
 
 
 def Rosenbrock(x):
     """Rosenbrock function for testing GaussNewton scheme"""
 
-    f = np.array([100*(x[1]-x[0]**2)**2+(1-x[0])**2])
+    f = 100*(x[1]-x[0]**2)**2+(1-x[0])**2
     g = np.array([2*(200*x[0]**3-200*x[0]*x[1]+x[0]-1), 200*(x[1]-x[0]**2)])
     H = np.array([[-400*x[1]+1200*x[0]**2+2, -400*x[0]], [-400*x[0], 200]])
     return f, g, H
@@ -118,10 +167,11 @@ def checkDerivative(fctn, x0, num=7, plotIt=True, dx=None):
     E0 = np.ones(t.shape)
     E1 = np.ones(t.shape)
 
+    l2norm = lambda x: np.sqrt(np.inner(x, x))  # because np.norm breaks if they are scalars?
     for i in range(num):
         Jt = fctn(x0+t[i]*dx)
-        E0[i] = norm(Jt[0]-Jc[0], 2)                     # 0th order Taylor
-        E1[i] = norm(Jt[0]-Jc[0]-t[i]*Jc[1].dot(dx), 2)  # 1st order Taylor
+        E0[i] = l2norm(Jt[0]-Jc[0])               # 0th order Taylor
+        E1[i] = l2norm(Jt[0]-Jc[0]-t[i]*Jc[1].dot(dx))  # 1st order Taylor
         order0 = np.log10(E0[:-1]/E0[1:])
         order1 = np.log10(E1[:-1]/E1[1:])
         print "%d\t%1.2e\t%1.3e\t\t%1.3e\t\t%1.3f" % (i, t[i], E0[i], E1[i], np.nan if i == 0 else order1[i-1])
@@ -156,9 +206,10 @@ def checkDerivative(fctn, x0, num=7, plotIt=True, dx=None):
 
 if __name__ == '__main__':
     x0 = np.array([2.6, 3.7])
-    fctn = lambda x:Rosenbrock(x)
-    checkDerivative(fctn, x0, plotIt=False)
-    xOpt = GaussNewton(fctn, x0, maxIter=20)
+    checkDerivative(Rosenbrock, x0, plotIt=False)
+    xOpt = GaussNewton(Rosenbrock, maxIter=20).minimize(x0)
+    print "xOpt=[%f, %f]" % (xOpt[0], xOpt[1])
+    xOpt = SteepestDescent(Rosenbrock, maxIter=20, maxIterLS=15).minimize(x0)
     print "xOpt=[%f, %f]" % (xOpt[0], xOpt[1])
 
     def simplePass(x):
